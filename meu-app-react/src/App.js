6b50fe3f76393c54f3e475acc0f5309dfc309855
adriastephanie@hotmail.com <adriastephanie@hotmail.com>
2021-03-27 22:34:43 -0300

    install react-route-dom and config route login and novousuario

--- meu-app-react/src/App.js
+++ meu-app-react/src/App.js
@@ -1,17 +1,20 @@
 import React, { Fragment } from 'react';
 import Navbar from './components/NavBar'
 import CarouselHome from './components/CarouselHome'
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+/*Páginas*/
+import Login from './view/login';
+import NovoUsuario from './view/usuario-novo';
+
 
 function App() {
-  return ( 
-    <Fragment>
-      <Navbar/>
-      <CarouselHome/>
-
-    </Fragment>
-
-  
- );
+  return(
+    <Router>
+      <Route exact path='/' component={Login} />
+      <Route exact path='/novousuario' component={NovoUsuario} />
+    </Router>
+  );
 }
 
 export default App;
