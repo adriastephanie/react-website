4bb1fc7ec9e0616686ae3c399d050234a91db19a
“Adria” <adriastephanie@hotmail.com>
2021-08-06 20:45:15 -0300

    chore: create login out

--- meu-app-react/src/components/NavBar/index.js
+++ meu-app-react/src/components/NavBar/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { Fragment } from 'react';
 
 export default function NavBar () {
+
+    const dispatch = useDispatch();
   
 
    return (
@@ -15,8 +19,23 @@ export default function NavBar () {
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <ul className="navbar-nav">
           <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-          <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
-          <li className="nav-item"><Link className="nav-link" to="/novousuario">Cadastrar</Link></li>
+          {
+            useSelector(state => state.usuarioLogado) > 0 ? 
+            <>
+              <li className="nav-item"><Link className="nav-link" to="#">Publicar Evento</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="#">Meus Eventos</Link></li>
+              <li className="nav-item"><Link className="nav-link" onClick={() => dispatch({type: 'LOG_OUT'})}>Sair</Link></li>
+              
+            </>
+            :
+            <>
+            <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/novousuario">Cadastrar</Link></li>
+            
+            </>
+
+            
+          } 
         </ul>
       </div>
   </nav>
