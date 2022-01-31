b3ff653ee77e89da4ae4c1a5af50408928b08492
“Adria” <adriastephanie@hotmail.com>
2022-01-31 12:06:17 -0300

    feat: css

--- meu-app-react/src/store/index.js
+++ meu-app-react/src/store/index.js
@@ -1,6 +1,19 @@
 import { createStore } from 'redux';
-import usuarioReducer from './usuarioReducer';
+import ussuarioReducer from './usuarioReducer';
+import { persistReducer, persisteStore } from 'redux-persist';
 
-const store = createStore(usuarioReducer);
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
 
-export default store;
+const persistConfig = {
+    key: 'siteeventos',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, ussuarioReducer);
+
+
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+export { store, persistor };
