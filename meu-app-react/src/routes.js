3a6ebee8424369bcfc71ca084bfc12e8961dfce6
“Adria” <adriastephanie@hotmail.com>
2022-01-31 12:06:42 -0300

    finish project

---
+++ meu-app-react/src/routes.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { store, persistor } from '../src/store/';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import Login from './view/login';
+import NovoUsuario from './view/usuario-novo';
+import Home from './view/home';
+import UsuarioRecuperarSenha from './view/recuperar-senha';
+import EventoCadastro from './view/evento-cadastro';
+import EventoDetalhes from './view/evento-detalhes';
+
+function Routes() {
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Router>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/eventos/' component={Home} />
+                    <Route exact path='/login' component={Login} />
+                    <Route exact path='/novousuario' component={NovoUsuario} />
+                    <Route exact path='/usuariorecuperarsenha' component={UsuarioRecuperarSenha} />
+                    <Route exact path='/eventocadastro' component={EventoCadastro} />
+                    <Route path='/eventodetalhes/:id' component={EventoDetalhes} />
+                    <Route path='/editarevento/:id' component={EventoCadastro} />
+                </Router>
+            </PersistGate>
+        </Provider>
+    );
+}
+export default Routes;
